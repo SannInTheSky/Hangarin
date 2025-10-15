5d0f1f3b712d15971411b5ed3f6a9be20fb22846
Grace Cornel <202380082@psu.palawan.edu.ph>
2025-10-16 07:06:16 +0800

    modified views.py

--- projectsite/tasks/views.py
+++ projectsite/tasks/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Task
 
-# Create your views here.
+class HomePageView(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = "home.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_tasks'] = Task.objects.count()
+        context['pending_tasks'] = Task.objects.filter(status='Pending').count()
+        context['completed_tasks'] = Task.objects.filter(status='Completed').count()
+        return context

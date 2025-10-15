6bc547bf8c2884924ffac1d74b6e301fb8893587
Grace Cornel <202380082@psu.palawan.edu.ph>
2025-10-16 07:51:36 +0800

    modified views

--- projectsite/tasks/views.py
+++ projectsite/tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Task
+from .models import Task, Category, Priority
 
 class HomePageView(ListView):
     model = Task
@@ -12,4 +12,24 @@ class HomePageView(ListView):
         context['total_tasks'] = Task.objects.count()
         context['pending_tasks'] = Task.objects.filter(status='Pending').count()
         context['completed_tasks'] = Task.objects.filter(status='Completed').count()
+        context['categories_count'] = Category.objects.count()
         return context
+
+class TaskListView(ListView):
+    model = Task
+    template_name = "task_list.html"
+    context_object_name = 'tasks'
+    paginate_by = 10
+    
+    def get_queryset(self):
+        return Task.objects.all().order_by('-created_at')
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = "category_list.html"
+    context_object_name = 'categories'
+
+class PriorityListView(ListView):
+    model = Priority
+    template_name = "priority_list.html"
+    context_object_name = 'priorities'
